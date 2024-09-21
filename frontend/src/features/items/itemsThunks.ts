036638b2a81b9d941259552edf5325b77f14d32b
DarkFlarex <darkflarexxx@gmail.com>
2024-09-21 18:04:47 +0600

    Добавил fetchOneItem и deleteItem. fetchOneItem для получения данных товара и её удаления. Реализована функциональность удаления товара с проверкой прав пользователя

--- frontend/src/features/items/itemsThunks.ts
+++ frontend/src/features/items/itemsThunks.ts
@@ -9,7 +9,45 @@ export const fetchItems = createAsyncThunk<Item[],string | undefined>(
     async (categoryId) => {
         const { data: items } = await axiosApi.get<Item[]>(`/items`,{params: {category: categoryId}});
         return items;
-    });
+});
+
+export const fetchOneItem = createAsyncThunk<Item, string>(
+    'items/fetchOne',
+    async (id) => {
+
+        const { data: item } = await axiosApi.get<Item>(`/items/${id}`);
+        return item;
+});
+
+export const deleteItem = createAsyncThunk<void, string, { rejectValue: GlobalError; state: RootState }>(
+    'items/fetchDelete',
+    async (id, { getState, rejectWithValue }) => {
+        const token = getState().users.user?.token;
+        const userId = getState().users.user?._id;
+
+        if (!token) {
+            return rejectWithValue({ error: 'User token is missing' });
+        }
+
+        try {
+            const { data: item } = await axiosApi.get<Item>(`/items/${id}`);
+
+            if (item.user !== userId) {
+                return rejectWithValue({ error: 'you do not have permission to delete the item' });
+            }
+
+
+            await axiosApi.delete(`/items/${id}`, {
+                headers: { 'Authorization': `Bearer ${token}` },
+            });
+        } catch (e) {
+            if (isAxiosError(e) && e.response) {
+                return rejectWithValue(e.response.data);
+            }
+            throw e;
+        }
+    }
+);
 
 export const createItem = createAsyncThunk<void, ItemMutation, { rejectValue: GlobalError; state: RootState }>(
     'items/create',
