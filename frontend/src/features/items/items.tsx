5139cb3e60dd2ca03cc264cebb4f05c18e7ddedf
DarkFlarex <darkflarexxx@gmail.com>
2024-09-21 15:54:32 +0600

    В компонент Items  отображается спискок товаров: реализован загрузчик и уведомление, если товаров нет

--- frontend/src/features/items/items.tsx
+++ frontend/src/features/items/items.tsx
@@ -1,19 +1,43 @@
-import {Grid, Typography} from "@mui/material";
+import {Alert, CircularProgress, Grid, Typography} from "@mui/material";
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {selectCategories} from "../categories/categoriesSlice";
 import CategoriesMenu from "../categories/components/CategoriesMenu";
 import {useEffect} from "react";
 import {fetchCategories} from "../categories/categoriesThunks";
+import {selectItems, selectItemsFetching} from "./itemsSlice";
+import {useParams} from "react-router-dom";
+import {fetchItems} from "./itemsThunks";
+import Itemitem from "./components/Itemitem";
 
 
 const Items = () => {
     const dispatch = useAppDispatch();
+    const items = useAppSelector(selectItems);
     const categories = useAppSelector(selectCategories);
-
+    const isFetching = useAppSelector(selectItemsFetching);
+    const {categoryId} = useParams();
 
     useEffect(() => {
+        dispatch(fetchItems(categoryId));
         dispatch(fetchCategories());
-    }, [dispatch]);
+    }, [dispatch,categoryId]);
+
+    let content:React.ReactNode = <Alert severity="info" sx={{width: '100%'}}>There are no Items here!</Alert>;
+
+    if(isFetching) {
+        content = <CircularProgress/>;
+    } else if (items.length > 0) {
+        content = items.map((item) => (
+            <Itemitem
+                key={item._id}
+                _id={item._id}
+                title={item.title}
+                price={item.price}
+                image={item.image}
+            />
+        ));
+    }
+
     return (
         <Grid container spacing={2}>
             <Grid item sx={{widht: 200}}>
@@ -24,6 +48,9 @@ const Items = () => {
                     <Grid item>
                         <Typography variant="h4">All items</Typography>
                     </Grid>
+                </Grid>
+                <Grid item container spacing={1}>
+                    {content}
                 </Grid>
             </Grid>
         </Grid>
